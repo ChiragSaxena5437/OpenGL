953719d86b87f532ae5cc0a224b5d64df3d35cf3
Chirag Saxena <saxenac7@gmail.com>
2020-05-07 22:31:13 +0530

    compiled ep 8

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -16,8 +16,8 @@ struct ShaderProgramSource
 //:::::::IT READS HERE:::::::
 static ShaderProgramSource ParseShader(const std::string& filepath)
 {
-    std::fstream stream(filepath);
-    
+    std::ifstream stream(filepath);
+
     enum class ShaderType
     {
         NONE = -1, VERTEX = 0, FRAGMENT = 1
@@ -39,32 +39,31 @@ static ShaderProgramSource ParseShader(const std::string& filepath)
             {
                 type = ShaderType::FRAGMENT;
             }
-            else
-            {
-                ss[(int)type] << line << "\n";
-            }
-
+        }
+        else
+        {
+            ss[(int)type] << line << "\n";
         }
     }
-    return{ ss[0].str(),ss[1].str() };
 
+    return{ ss[0].str(),ss[1].str() };
 }
-static unsigned int CompileShader( unsigned int type, const std::string& source)
+static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
-    unsigned int id= glCreateShader(type);
+    unsigned int id = glCreateShader(type);
     const char* src = source.c_str();
     glShaderSource(id, 1, &src, nullptr);
     glCompileShader(id);
 
-    int result;  
+    int result;
     glGetShaderiv(id, GL_COMPILE_STATUS, &result);
     if (result == GL_FALSE)
     {
         int length;
         glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = (char*)_malloca(length * sizeof(char));
+        char* message = (char*)alloca(length * sizeof(char));
         glGetShaderInfoLog(id, length, &length, message);
-        std::cout << "Failed to complie"<<(type == GL_VERTEX_SHADER ? "vertex": "fragment")<<"shader " << std::endl;
+        std::cout << "Failed to complie" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader " << std::endl;
         std::cout << message << std::endl;
         glDeleteShader(id);
         return 0;
@@ -78,7 +77,7 @@ static unsigned int CompileShader( unsigned int type, const std::string& source)
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
 {
     unsigned int program = glCreateProgram();
-    unsigned int  vs = CompileShader( GL_VERTEX_SHADER , vertexShader);
+    unsigned int  vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
     unsigned int  fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
     glAttachShader(program, vs);
@@ -100,7 +99,7 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-    
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -127,18 +126,18 @@ int main(void)
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(float)*2,0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     //:::::::AND PRINTS HERE:::::::
     std::cout << "VERTEX" << std::endl;
-    std::cout <<source.VertexSource << std::endl;
+    std::cout << source.VertexSource << std::endl;
     std::cout << "FRAGMENT" << std::endl;
     std::cout << source.FragmentSource << std::endl;
 
 
-    //unsigned int shader = CreateShader(vertexShader,fragmentShader);
-    //glUseProgram(shader);
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+    glUseProgram(shader);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -147,9 +146,9 @@ int main(void)
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        glDrawArrays(GL_TRIANGLES,0,3);
-       
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
@@ -157,7 +156,7 @@ int main(void)
         glfwPollEvents();
     }
 
-    //glDeleteProgram(shader);
+    glDeleteProgram(shader);
     glfwTerminate();
     return 0;
 }
