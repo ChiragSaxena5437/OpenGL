9cfeaa02a7afb93933d3560bdaec4b3560493a64
Chirag Saxena <saxenac7@gmail.com>
2020-06-21 18:15:42 +0530

    This is the code with the error

--- OpenGL/Application.16.cpp
+++ OpenGL/Application.16.cpp
@@ -6,20 +6,17 @@
 #include <sstream>
 //16    
 #define ASSERT(x) if (!(x)) __debugbreak();
-#define GLCall(x) GLClearError();\
-    x;\
-    ASSERT(GLLogCall(#x,__FILE__,__LINE__))
+#define GLCall(x) GlClearError();\
 static void GLClearError()
 {
-    while (glGetError() != GL_NO_ERROR);
+while (glGetError() != GL_NO_ERROR);
 }
 
-static bool GLLogCall(const char* function, const char* file, int line)
+static bool GLLogCall()
 {
     while (GLenum error = glGetError())
     {
-        std::cout << "[OpenGl Error] (" << error << ")" << function <<
-        " "<< file << ";"<< line <<    std::endl;
+        std::cout << "[OpenGl Error] ( " << error << ")" << std::endl;
         return false;
     }
     return true;
@@ -127,8 +124,6 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-
-    glfwSwapInterval(1);
     if (glewInit() != GLEW_OK)
         std::cout << "Error" << std::endl;
     std::cout << glGetString(GL_VERSION) << std::endl;
@@ -140,62 +135,43 @@ int main(void)
          -0.5f, 0.5f,
 
     };
-     
     unsigned int indicies[] = {
         0,1,2,
-        2,3,0 
+        2,3,0
     };
     unsigned int buffer;
-    GLCall(glGenBuffers(1/*1=no of buffers req*/, &buffer));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-    GLCall(glBufferData(GL_ARRAY_BUFFER, 4* 2 * sizeof(float), positions, GL_STATIC_DRAW));
+    glGenBuffers(1/*1=no of buffers req*/, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
-    GLCall(glEnableVertexAttribArray(0));
-    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
-                                           
-    unsigned int ibo ;
-    GLCall(glGenBuffers(1/*1=no of buffers req*/, &ibo));
-    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4 * 2 * sizeof(float), indicies, GL_STATIC_DRAW));
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-    ShaderProgramSource source  = ParseShader("res/shaders/Basic.shader");
+    unsigned int ibo;
+    glGenBuffers(1/*1=no of buffers req*/, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * 2 * sizeof(float), indicies, GL_STATIC_DRAW);
+
+    ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
+    //:::::::AND PRINTS HERE:::::::
+    std::cout << "VERTEX" << std::endl;
+    std::cout << source.VertexSource << std::endl;
+    std::cout << "FRAGMENT" << std::endl;
+    std::cout << source.FragmentSource << std::endl;
+
+
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    GLCall(glUseProgram(shader));
+    glUseProgram(shader);
 
-    int location = glGetUniformLocation(shader, "u_Color");
-    ASSERT(location != -1);
-   glUniform4f(location,0.2f,0.8f,0.8f,1.0f);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-   GLCall(glUseProgram(0));
-   GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-   GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
-
-    float  r = 0.0f;
-    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        GLCall(glClear(GL_COLOR_BUFFER_BIT));
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        GLCall(glUseProgram(shader));
-        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
-
-        GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer)); 
-        GLCall(glEnableVertexAttribArray(0));
-        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
-        
-
-        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-        
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,nullptr));
-       
-        if (r > 1.0f)
-            increment = -0.05f;
-        else if (r < 0.0f)
-            increment = 0.05f;
-
-        r += increment;
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
