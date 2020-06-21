d17ccac7cbde34b6ca2627ef85671e0adc614b0d
Chirag Saxena <saxenac7@gmail.com>
2020-06-21 19:12:06 +0530

    At Last This is what it looks finally

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -19,7 +19,7 @@ static bool GLLogCall(const char* function, const char* file, int line)
     while (GLenum error = glGetError())
     {
         std::cout << "[OpenGl Error] (" << error << ")" << function <<
-        " "<< file << ";"<< line <<    std::endl;
+            " " << file << ";" << line << std::endl;
         return false;
     }
     return true;
@@ -141,58 +141,40 @@ int main(void)
          -0.5f, 0.5f,
 
     };
-     
+
     unsigned int indicies[] = {
         0,1,2,
-        2,3,0 
+        2,3,0
     };
     unsigned int buffer;
 
     GLCall(glGenBuffers(1/*1=no of buffers req*/, &buffer));
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-    GLCall(glBufferData(GL_ARRAY_BUFFER, 4* 2 * sizeof(float), positions, GL_STATIC_DRAW));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
 
     GLCall(glEnableVertexAttribArray(0));
     GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
-                                           
-    unsigned int ibo ;
+
+    unsigned int ibo;
     GLCall(glGenBuffers(1/*1=no of buffers req*/, &ibo));
     GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
     GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4 * 2 * sizeof(float), indicies, GL_STATIC_DRAW));
 
-    ShaderProgramSource source  = ParseShader("res/shaders/Basic.shader");
+    ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
 
     int location = glGetUniformLocation(shader, "u_Color");
     ASSERT(location != -1);
-   glUniform4f(location,0.2f,0.8f,0.8f,1.0f);
+    glUniform4f(location, 0.2f, 0.8f, 0.8f, 1.0f);
 
-   GLCall(glUseProgram(0));
-   GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-   GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
 
     float  r = 0.0f;
     float increment = 0.05f;
-    glGenBuffers(1/*1=no of buffers req*/, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(float)*2,0);
-
-    ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-    //:::::::AND PRINTS HERE:::::::
-    std::cout << "VERTEX" << std::endl;
-    std::cout <<source.VertexSource << std::endl;
-    std::cout << "FRAGMENT" << std::endl;
-    std::cout << source.FragmentSource << std::endl;
-
-
-    unsigned int shader = CreateShader(vertexShader,fragmentShader);
-    glUseProgram(shader);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+   
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -203,15 +185,15 @@ int main(void)
         GLCall(glUseProgram(shader));
         GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
-        GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer)); 
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
         GLCall(glEnableVertexAttribArray(0));
         GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
-        
+
 
         GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-        
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,nullptr));
-       
+
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
         if (r > 1.0f)
             increment = -0.05f;
         else if (r < 0.0f)
