ccc82cc036281c0cf87565e6081aaf2dacf24378
Chirag Saxena <saxenac7@gmail.com>
2020-06-26 07:12:47 +0530

    Ep 21 (Though you dont need imgui in this episode but you will need in the next episode still i have downloaded it)

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -11,8 +11,9 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.inl"
 #include <glm\ext\matrix_clip_space.hpp>
+#include <glm\gtx\transform.hpp>
 int main(void)
-{
+{   //Episode 21 
     GLFWwindow* window;
     /* Initialize the library */
     if (!glfwInit())
@@ -22,7 +23,7 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
     /* Create a windowed mode window and its OpenGL context */ 
-    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "Application", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -41,7 +42,7 @@ int main(void)
         float positions[] = {
             100.0f, 100.0f, 0.0f, 0.0f, // 0
             200.0f, 100.0f, 1.0f, 0.0f, // 1
-            200.0f, 200.0f, 1.0f, 1.0f,// 2
+            200.0f, 200.0f, 1.0f, 1.0f, // 2
             100.0f, 200.0f, 0.0f, 1.0f  // 3
         };   
 
@@ -49,59 +50,61 @@ int main(void)
             0, 1, 2,
             2, 3, 0
         };
+        
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+        VertexArray va;
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+        IndexBuffer ib(indices, 6);
+
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
+
+        glm::mat4 proj = glm::ortho(0.0f,960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f),glm::vec3(-100, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+        glm::mat4 mvp = model * view * proj ;
+
+        Shader shader("res/shaders/Basic.shader");
+        shader.Bind();
+        shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+        shader.SetUniform4f("u_MVP", mvp);
+        //mvp=Model View Projection
+        
+        Renderer renderer;
+        float r = 0.0f;
+        float increment = 0.05f;
+        while (!glfwWindowShouldClose(window))
         {
-            GLCall(glEnable(GL_BLEND));
-            GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-            VertexArray va;
-            VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-            IndexBuffer ib(indices, 6);
-
-            VertexBufferLayout layout;
-            layout.Push<float>(2);
-            layout.Push<float>(2);
-            va.AddBuffer(vb, layout);
-
-            glm::mat4 proj = glm::ortho(0.0f,960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-            glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
-            glm::vec4 result = proj * vp;
-
-            Shader shader("res/shaders/Basic.shader");
+             
+            renderer.Clear();
             shader.Bind();
-            shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-            shader.SetUniform4f("u_MVP", proj);
+            shader.SetUniform4f("u_Color", r, 0.3f, 1.5f, 1.0f);
             
-            Renderer renderer;
-            float r = 0.0f;
-            float increment = 0.05f;
-            while (!glfwWindowShouldClose(window))
-            {
-                        /* Render here */
-                renderer.Clear();
-                shader.Bind();
-                shader.SetUniform4f("u_Color", r, 0.3f, 1.5f, 1.0f);
-                
-                Texture texture("res/textures/chirag.png");
-                texture.Bind();
-                shader.SetUniform1i("u_Texture", 0);
+            Texture texture("res/textures/chirag.png");
+            texture.Bind();
+            shader.SetUniform1i("u_Texture", 0);
 
-                renderer.Draw(va, ib, shader);
-                
-                if (r > 1.0f)
-                    increment = -0.05f;
-                else if (r < 0.0f)
-                    increment = 0.05f;
+            renderer.Draw(va, ib, shader);
+            
+            if (r > 1.0f)
+                increment = -0.05f;
+            else if (r < 0.0f)
+                increment = 0.05f;
 
-                r += increment;
+            r += increment;
 
-                /* Swap front and back buffers */
-                glfwSwapBuffers(window);
-                
-                /* Poll for and process events */
-                glfwPollEvents();
-            }
-
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+            
+            /* Poll for and process events */
+            glfwPollEvents();
         }
+
+        
      }
     // Close OpenGL window and terminate GLFW
     glfwTerminate();
